6af8efdbb267c0a17197518a15f701cbc19e539f
Setting Sun <richardaustinhall@gmail.com>
2021-12-08 23:27:10 -0800

    adding moment npm

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 
-var startDate = new Date(2012, 4, 1);
-var endDate = new Date(2012, 4, 23);
-var dateRange = moment().range(startDate, endDate);
 
-var days = [1, 2, 3, 4, 5]; // mon-fri
-var startTime = moment('09:00 +0000', 'HH:mm Z'); // 9:00 am UTC
-var endTime = moment('17:00 +0000', 'HH:mm Z'); // 5:00 pm UTC
-var timeRange = moment().range(startTime, endTime);
 
-function isWithin(dateRange, timeRange, days, when) {
-  return (when.within(dateRange) && !!~days.indexOf(when.day()) && moment(when.format('HH:mm Z'), 'HH:mm Z').within(timeRange));
-}
-
-var when = moment('2012-05-22 12:32 +0000', 'YYYY-MM-DD HH:mm Z');
-
-console.log(isWithin(dateRange, timeRange, days, when));
+// import moment from 'moment';
+// console.log(moment().format('DD')): 
